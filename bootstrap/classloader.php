dd59b0a8b8482f1fd6afb6c13ac6fbb78f251a54
jtarleton <root@server.crystalbit.com>
2012-11-23 13:11:52 +0000

    revise include path

--- bootstrap/classloader.php
+++ bootstrap/classloader.php
@@ -1,4 +1,62 @@
 <?php 
 /**
 * Classloader
-*.
+*/
+
+// session_start();
+date_default_timezone_set('America/New_York');
+
+//CONFIG
+foreach (glob(DOCROOT.'config/*.php') as $filename)
+{
+    require_once($filename);
+}
+
+//OUTLET DB
+foreach (glob(DOCROOT.'outlet/*.php') as $filename)
+{
+    require_once($filename);
+}
+
+//LIB
+foreach (glob(DOCROOT.'lib/*.php') as $filename)
+{
+    require_once($filename);
+}
+
+
+//MODEL
+foreach (glob(DOCROOT.'app1/models/*.php') as $filename)
+{
+    require_once($filename);
+}
+
+
+//VIEW
+foreach (glob(DOCROOT.'app1/layout/*.php') as $filename)
+{
+    require_once($filename);
+}
+
+foreach (glob(DOCROOT.'app1/views/*.php') as $filename)
+{
+    require_once($filename);
+}
+
+
+//CONTROLLER
+foreach (glob(DOCROOT.'app1/controllers/*.php') as $filename)
+{
+    require_once($filename);
+}
+
+
+
+
+
+
+
+
+
+
+
