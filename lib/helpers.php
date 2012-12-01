a91faa4623c08977b7a063d476eb92f2c3c7d1ad
jtarleton <root@server.crystalbit.com>
2012-12-01 18:45:37 +0000

    add stuff

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,6 +1,12 @@
 <?php
 
-function link_to($href='#', $display='Click Me', array $opts)
+function link_to($href='#', $display='Click Me', array $opts=array(
+'class'=>null,
+'id'=>null,
+'href'=>null,
+'target'=>null,
+'title'=>null
+))
 {
 	foreach($opts as $opt=>&$value)
 		${$opt} = ( !isset($value) ) ? '' : $value;
@@ -14,3 +20,10 @@ function link_to($href='#', $display='Click Me', array $opts)
 		$display );	
 }
 
+function form_tag($action='', $method='POST', array $opts=array())
+{
+	if(!isset($opts['class'])) $opts['class'] = sprintf( 'class-%s', uniqid() );
+	if(!isset($opts['id'])) $opts['id'] = sprintf( 'id-%s', uniqid() );
+	
+	return sprintf('<form action="%s" method="%s" class="%s" id="%s">', $action, $method, $opts['class'], $opts['id']);	
+}
--- public/index.php
+++ public/index.php
@@ -7,4 +7,8 @@ session_start();
 require_once('/var/project/bootstrap/classloader.php'); 
 $p = strip_tags($_GET['p']);
 
-ActionController::getInstance()->dispatch($p);
+//ActionController::getInstance()->dispatch($p);
+echo form_tag();
+	
+?><input type="submit" value="GO"></input></form><?php
+	echo link_to('HERE','http://www.bing.com');
