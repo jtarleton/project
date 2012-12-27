2ad3eb4cd487aee848c4b7f5de9e879430352ac9
jtarleton <jamestarleton@gmail.com>
2012-12-27 20:12:55 +0000

    add stuff

--- app1/models/WpPost.class.php
+++ app1/models/WpPost.class.php
@@ -3,10 +3,107 @@
 class WpPost extends BaseObject
 {
 
-	public function __construct()
+
+	public $data;
+
+	public function __construct($id= null)
 	{
 
 		parent::__construct();
+
+		if(isset($id))
+		{
+		
+			$this->load($id);
+		}
+
+	}
+
+
+	public function load($id)
+	{
+		$doc = self::retrieveByPK($id);
+
+		
+
+		$this->data['_id'] = (int)$doc['_id'];
+		$this->data['post_title'] = $doc['post_title'];
+		$this->data['post_content'] = $doc['post_content'];
+		$this->data['post_slug'] = $doc['post_slug'];
+
+	}
+
+	static public function getAutoInc() 
+	{
+	
+		$obj = MongoFactory::MongoCreate();
+		$docs = $obj->test->wp_post->find();
+		$ids = array();
+		foreach($docs as $doc){
+			$ids[] = (int)$doc['_id'];
+		}
+		$inc = max($ids) + 1;
+		return $inc;
+	}
+
+	static public function deleteByPK($id) 
+	{
+		$obj = new self($id);
+		$n = $obj->delete();
+		if($n > 0) {
+			return $n;
+		} else {
+			throw new Exception('Error deleting record by id '. $id );
+		}
+	}
+
+	public function delete() 
+	{
+		
+		$ret =$this->db->test->wp_post->remove(array('_id' => $this->getId()), array('safe'=>true, "justOne" => true));
+		return $ret['n'];
+
+	}
+
+	static public function createNew($postdata)
+	{
+
+		$obj = new self;
+		$id = self::getAutoInc();
+		
+		$postdata['_id'] = (int) $id;
+
+
+
+		$obj->setAttributes($postdata);
+		$obj->save();
+		return $obj;
+		
+	}
+
+	public function getId() 
+	{
+		$attrs = $this->getAttributes();
+		$id = (int)$attrs['_id'];
+		return $id;
+	
+	}
+	public function getAttributes(){
+		return $this->data;
+	}
+	public function save()
+	{
+		$doc = array();
+		foreach($this->getAttributes() as  $k=>$v){
+			$doc[$k] = $v;
+		}
+		$this->db->test->wp_post->insert($doc);
+	
+	}
+
+	public function setAttributes($data) 
+	{
+		foreach( $data as $k=>$v) $this->data[$k] = $v;
 	}
 
 
@@ -29,28 +126,56 @@ class WpPost extends BaseObject
 
 
 		$self = self::getInstance();
-                $post = ( isset( $pid) ) ? $self->db->test->wp_post->findOne(array('_id'=>(int)$pid) ) : null;
+                $post = ( isset( $pid) ) ? $self->db->test->wp_post->findOne(array('_id'=>(int)$pid ) ) : null;
 	
 
-		if(!isset( $post ) ) throw new Exception('Post not found'. $pk);
+		if(!isset( $post ) ) throw new Exception('Post not found for id '. $pid);
 
-		return (array ) $post;
+		return (array) $post;
 
 
 	}
 
-	static public function retrieveAll( $s = false, $limit = 10)
+
+
+	static public function retrieveAll($s = 0, $limit=2)
+	{
+
+
+
+               $offset = (!empty ( $s ) )  ?  ($s - 1 )* $limit  : 0;
+
+                $self = self::getInstance();
+                //$posts = ($s) ? 
+
+                $posts =        $self->db->test->wp_post->find()->sort(array('_id'=>-1))->skip( $offset )->limit( $limit );
+                         //$self->db->test->wp_post->find();
+
+                return $posts;
+
+
+
+
+
+
+
+
+
+	}
+
+
+	static public function retrieveLive( $s = 0, $limit = 2)
 	{
 
 		 
 		
 
-		$offset = ($s - 1 )* $limit;
+		$offset = (!empty ( $s ) )  ?  ($s - 1 )* $limit  : 0;
 
 		$self = self::getInstance();
 		//$posts = ($s) ? 
 
-		$posts =	$self->db->test->wp_post->find()->sort(array('_id'=>-1))->skip( $offset )->limit( $limit );
+		$posts =	$self->db->test->wp_post->find(array('post_status'=>'publish'))->sort(array('_id'=>-1))->skip( $offset )->limit( $limit );
 			 //$self->db->test->wp_post->find();
 		
 		return $posts;
@@ -58,31 +183,7 @@ class WpPost extends BaseObject
 		foreach($posts as $post)
 		{
 
-
-		
-
 		}
-		
-
-
-
-
-		 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	}
 
 
--- app1/views/aboutSuccess.php
+++ app1/views/aboutSuccess.php
@@ -1,4 +1,20 @@
 <div id="content"><h2>About</h2>
+
+<p>
+
+Crystal Bit is my personal home page and original web application, written in PHP 5.4, based on the <a href="http://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller">MVC architecture</a>, and driven by the <a href="http://www.mongodb.org">MongoDB</a> database.  
+</p>
+<p>
+You'll notice this site does not use a certain <a href="http://www.wordpress.org">popular blog application</a>, nor does it use any of the 
+many good <a href="http://en.wikipedia.org/wiki/Comparison_of_web_application_frameworks#PHP_2">PHP frameworks</a> out there; rather, it is a framework of my own design.  If you code, feel 
+free to <a href="https://github.com/jtarleton/project/fork">fork</a> this project on github.
+</p>
+
+<p>
+-James
+</p>
+
+
 <ul><?php
 echo "<li>Last modified: " . date ('l, F j\<\s\u\p\>S\<\/\s\u\p\>, Y \a\t g:ia', getlastmod()) .'</li>';;
 
