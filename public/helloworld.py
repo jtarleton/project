55dec853a4f02305a92bce023228e60b713eaeee
jtarleton <jamestarleton@gmail.com>
2013-01-25 02:36:23 +0000

    add stuff

---
+++ public/helloworld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+print "Content-Type: text/html\n\n"
+def generate_page():
+print "<HTML>\n"
+print "<HEAD>\n"
+print "\t<TITLE> My Python HTML Demo</TITLE>\n"
+print "</HEAD>\n"
+print "<BODY BGCOLOR =#F7FCAE>\n"
+print "\t<H1>Hello World</H1>\n"
+print "</BODY>\n"
+print "</HTML>\n"
+generate_page()
