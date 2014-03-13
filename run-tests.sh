804d70815f698cf9eb9ed99a5f1d6b55c4a1997f
jtarleton <jamestarleton@gmail.com>
2014-03-13 16:33:21 -0400

    update travis.yml

--- run-tests.sh
+++ run-tests.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+retfunc()
+{
+    echo "this is retfunc()"
+    return 1
+}
+
+exitfunc()
+{
+    echo "this is exitfunc()"
+    exit 0
+}
+
+retfunc
+echo "We are still here"
+exitfunc
+echo "We will never see this"
