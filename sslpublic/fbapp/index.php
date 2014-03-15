7e32d7a952d9693e7093cf7ca5ef631e1085809f
James Tarleton <jamestarleton@gmail.com>
2014-03-15 17:30:36 -0400

    updates/move stuff out of non-ssl

---
+++ sslpublic/fbapp/index.php
@@ -0,0 +1,17 @@
+<pre>Welcome to James's Unix Timestamp / Date Converter brought to you by CrystalBit.
+
+To begin, please select the type of conversion and the date or timestamp to convert.
+
+UNIX timestamp to date
+
+ts
+
+The date is 
+
+
+Date to UNIX Timestamp
+
+date
+
+The UNIX timestamp is
+</pre>
