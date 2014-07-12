2120086d6ca534e60aecb37d03c3ad46183501a4
James Tarleton <jamestarleton@gmail.com>
2014-07-12 15:56:04 -0400

    update constants

--- app1/controllers/actioncontroller2.class.php
+++ app1/controllers/actioncontroller2.class.php
@@ -4,13 +4,8 @@
  */
 class ActionController2
 {
-	static private $instance; private $p, $outlet;
-
-
-
-
-
-
+	static private $instance; 
+	private $p, $outlet;
 
 	public function __construct($p){
 	
@@ -72,60 +67,12 @@ class ActionController2
 		
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	public function executeCreatethelink( )
         {
                 $clean = array();
                 foreach($_POST as $k=>$v) $clean[$k] = strip_tags($v);
                 $p = WpLink::createNew($clean);
-                if($p) header('Location: '.BASEHREF.'index.php?p=index'); 
+                if($p) header('Location: '.SBASEHREF.'sindex.php?p=index'); 
 	}
       
 
@@ -138,7 +85,7 @@ class ActionController2
                         'url'=>$url,
                 );
 		$mongo->test->wp_link->update(array('_id'=>new MongoId($linkid) ), array('$set'=>$updates));
-		header('Location: '.BASEHREF.'index.php?p=index');
+		header('Location: '.SBASEHREF.'sindex.php?p=index');
 	}
 
 
@@ -148,99 +95,83 @@ class ActionController2
                 $clean = array();
                 foreach($_POST as $k=>$v) $clean[$k] = strip_tags($v);
                 $p = WpTerm::createNewTag($clean);
-                if($p) header('Location: '.BASEHREF.'index.php?p=index'); 
+                if($p) header('Location: '.SBASEHREF.'sindex.php?p=index'); 
 	}
         
 	public function executeEditthetag( )
-        {       $post = $_POST; //TODO .... put in model
-                foreach($post as $k=>$v ) $$k=$v;
-                $mongo=MongoFactory::MongoCreate();
-                $updates = array(
-                        'name'=>$name,
+	{       
+		$post = $_POST; //TODO .... put in model
+		foreach($post as $k=>$v ) $$k=$v;
+		$mongo=MongoFactory::MongoCreate();
+		$updates = array(
+			'name'=>$name,
 			'slug'=>$slug
-                );
+		);
 		$mongo->test->wp_term->update(array('_id'=>new MongoId($tagid) ), array('$set'=>$updates));
-		header('Location: '.BASEHREF.'index.php?p=index');
+		header('Location: '.SBASEHREF.'sindex.php?p=index');
 	}
 
 
 	public function executeCreatec( )
-        {
-                $clean = array();
-                foreach($_POST as $k=>$v) $clean[$k] = strip_tags($v);
-                $p = WpTerm::createNewCat($clean);
-                if($p) header('Location: '.BASEHREF.'index.php?p=index'); }
+	{
+		$clean = array();
+		foreach($_POST as $k=>$v) $clean[$k] = strip_tags($v);
+		$p = WpTerm::createNewCat($clean);
+		if($p) header('Location: '.SBASEHREF.'sindex.php?p=index'); 
+    }
         
 
 	public function executeEditthecomm( )
-        {       $post = $_POST; //TODO .... put in model
-                foreach($post as $k=>$v ) $$k=$v;
-                $mongo=MongoFactory::MongoCreate();
-                $updates = array(
-                        'commenttext'=>$commenttext,
-                );
+    {       
+        $post = $_POST; //TODO .... put in model
+        foreach($post as $k=>$v ) $$k=$v;
+        	$mongo=MongoFactory::MongoCreate();
+        	$updates = array(
+                'commenttext'=>$commenttext,
+        	);
 		$mongo->test->wp_comment->update(array('_id'=>(int)$commid), array('$set'=>$updates));
 
-		header('Location: '.BASEHREF.'admin.php?p=comments');}
+		header('Location: '.SBASEHREF.'admin.php?p=comments');
+	}
 
 
 
 
 
 
-public function executeEditthecat( )
-        {       $post = $_POST; //TODO .... put in model
-                foreach($post as $k=>$v ) $$k=$v;
-                $mongo=MongoFactory::MongoCreate();
-                $updates = array(
-                        'name'=>$name,
-                );
-$mongo->test->wp_term->update(array('_id'=>new MongoId($catid) ), array('$set'=>$updates));header('Location: '.BASEHREF.'index.php?p=index');}
-
-
-
-
-
-
-
-
-
-
-
-
+	public function executeEditthecat( )
+	{       
+		$post = $_POST; //TODO .... put in model
+		foreach($post as $k=>$v ) $$k=$v;
+		$mongo=MongoFactory::MongoCreate();
+		$updates = array(
+		'name'=>$name,
+		);
+		$mongo->test->wp_term->update(array('_id'=>new MongoId($catid) ), array('$set'=>$updates));
+		header('Location: '.SBASEHREF.'sindex.php?p=index');
+	}
 
 	public function executeCreatep( )
 	{
 		$clean = array();
 		foreach($_POST as $k=>$v) $clean[$k] = strip_tags($v);
 		$p = WpPost::createNew($clean);
-		if($p) header('Location: '.BASEHREF.'admin.php?p=edit&pid=' .$p->getId() );	}
+		if($p) header('Location: '.SBASEHREF.'admin.php?p=edit&pid=' .$p->getId() );	
+	}
 
-
-
-
-
-
-
-
-
-
-	    public function executeEditp( )
+	public function executeEditp( )
     {   
 
         $post = $_POST;
         //foreach($post as $k=>$v )
         //$$k=utf8_encode($v);
 
-	$tags = $_POST['tags'];
-        $cats=	$_POST['cats'];
-	$post_title = $_POST['post_title'];
-	$post_content = $_POST['post_content'];
-	$post_status = $_POST['post_status'];
-	$pid = (int)$_POST['pid'];
-
-
-
+		$tags = $_POST['tags'];
+		$cats=	$_POST['cats'];
+		$post_title = $_POST['post_title'];
+		$post_content = $_POST['post_content'];
+		$post_status = $_POST['post_status'];
+		$pid = (int)$_POST['pid'];
 
         $mongo=MongoFactory::MongoCreate();
         $updates = array(
@@ -278,11 +209,11 @@ $mongo->test->wp_term->update(array('_id'=>new MongoId($catid) ), array('$set'=>
         foreach($cats as $term_id)
         {
                 
-                $tts=WpTermTaxonomy::retrieveByTermId($term_id);
-                
-                foreach($tts as $tt) 
-                {
-                
+            $tts=WpTermTaxonomy::retrieveByTermId($term_id);
+            
+            foreach($tts as $tt) 
+            {
+            
                 $doc = array(
                 'object_id'=>(int)$pid,
                 'term_taxonomy_id'=>$tt->getId()->{'$id'} //getTaxFromTermId$term_id
@@ -291,10 +222,7 @@ $mongo->test->wp_term->update(array('_id'=>new MongoId($catid) ), array('$set'=>
             }
         }
     
-    
-    
-    
-    /* 
+    	/* 
         foreach($tags as $term_id)
         {
             //get term tax id for termid
@@ -316,85 +244,57 @@ $mongo->test->wp_term->update(array('_id'=>new MongoId($catid) ), array('$set'=>
         if($retval['ok'])
         {
             if($post_status=='publish')
-                header(sprintf('Location: http://www.crystalbit.com/index.php?p=%s&pid=%s','postdetail', $pid));
+                header(sprintf('Location: '.SBASEHREF.'sindex.php?p=%s&pid=%s','postdetail', $pid));
             else
-                header(sprintf('Location: http://www.crystalbit.com/admin.php?p=%s','index')); //&pid=%s',$pid))
+                header(sprintf('Location: '.SBASEHREF.'admin.php?p=%s','index')); //&pid=%s',$pid))
         }
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	public function executeDeltag()
-        {
-                $tid = $_GET['tagid'];
-                $ok = WpTerm::deleteByPK($tid);
-                if($ok > 0)
-                        header('Location: '.BASEHREF.'admin.php?p=tags'); // admin.php?p=links
+    {
+        $tid = $_GET['tagid'];
+        $ok = WpTerm::deleteByPK($tid);
+        if($ok > 0)
+                header('Location: '.SBASEHREF.'admin.php?p=tags'); // admin.php?p=links
 
-        }
+    }
 
 	public function executeDelcat()
-        {
-                $cid = $_GET['catid'];
-                $ok = WpTerm::deleteByPK($cid);
-                if($ok > 0)
-                        header('Location: '.BASEHREF.'admin.php?p=categories'); // admin.php?p=links
+    {
+        $cid = $_GET['catid'];
+        $ok = WpTerm::deleteByPK($cid);
+        if($ok > 0)
+                header('Location: '.SBASEHREF.'admin.php?p=categories'); // admin.php?p=links
 
-        }
+    }
 
 	public function executeDell()
-        { 
+	{ 
 		$id = $_GET['linkid'];
-        	$ok = WpLink::deleteByPK($id);
-        	if($ok > 0)
-                	header('Location: '.BASEHREF.'admin.php?p=links'); // admin.php?p=links
-		
+		$ok = WpLink::deleteByPK($id);
+		if($ok > 0)
+	    	header('Location: '.SBASEHREF.'admin.php?p=links'); // admin.php?p=links
+
 	}
 
+	public function executeDelcomm()
+    {
+        $id = $_GET['commid'];
+        $ok = WpComment::deleteByPK($id);
+        if($ok > 0)
+                header('Location: '.SBASEHREF.'admin.php?p=comments'); // admin.php?p=links
 
-
-
-	 public function executeDelcomm()
-        {
-                $id = $_GET['commid'];
-                $ok = WpComment::deleteByPK($id);
-                if($ok > 0)
-                        header('Location: '.BASEHREF.'admin.php?p=comments'); // admin.php?p=links
-
-        }
+    }
 	
 	public function executeDelp() 
 	{
-	
-	$id = $_GET['pid'];
-	$ok = WpPost::deleteByPK($id);
-	if($ok > 0) 
-		header('Location: '.BASEHREF.'admin.php?p=index&s=1');
+		
+		$id = $_GET['pid'];
+		$ok = WpPost::deleteByPK($id);
+		if($ok > 0) 
+			header('Location: '.SBASEHREF.'admin.php?p=index&s=1');
 	}
-
-
-
-
-
-
-
-
-
 
 	static private function passAuth($email, $pass)
 	{
@@ -412,7 +312,7 @@ $mongo->test->wp_term->update(array('_id'=>new MongoId($catid) ), array('$set'=>
 	private function executeLogout()
 	{ 
 		$_SESSION['isAuth2'] = false; 
-		header('Location: '.BASEHREF.'');
+		header('Location: '.SBASEHREF.'');
 		exit(0);
 	}
 	private function executeAuthcallback()
@@ -435,31 +335,14 @@ $mongo->test->wp_term->update(array('_id'=>new MongoId($catid) ), array('$set'=>
 
 			session_write_close(); 
 
-			header('Location: '.BASEHREF);
+			header('Location: '.SBASEHREF);
 			exit(0);
 			
 		}
 		else
 		{
-			header('Location: '.BASEHREF.'index.php?p=login'); exit(0);
+			header('Location: '.SBASEHREF.'sindex.php?p=login'); exit(0);
 		}
 		
 	}
-
-
-
-
-
-
-
-
-
-	
-
 }
-
-
-
-
-
-
