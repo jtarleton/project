a3a457a4450a04a6fa2fa8d55109f95f0f98a6b4
James Tarleton <jamestarleton@gmail.com>
2014-08-06 19:26:20 -0400

    updates

---
+++ app1/views/sunriseSuccess.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+
+$time = new DateTime('now', new DateTimeZone('America/New_York'));
+
+$sts = date_sunrise(
+$time->getTimestamp(),
+SUNFUNCS_RET_TIMESTAMP,
+40.691502,
+-73.805702,
+90.2605,
+$time->getOffset() / 3600
+)
+;
+
+echo 'Sunrise: <br />'.
+date('l jS \of F Y ',  $sts);
+?>
+<br />
+<?php 
+
+echo date('h:i:s A', $sts);
