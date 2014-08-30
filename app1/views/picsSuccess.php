10fe3ad46f3dc35a667e2c395ba904f84fbd56bb
James Tarleton <jamestarleton@gmail.com>
2014-08-30 17:40:20 -0400

    update

--- app1/views/picsSuccess.php
+++ app1/views/picsSuccess.php
@@ -1,28 +1,44 @@
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="
+https://www.crystalbit.com/images/mypics/beautiful-scenery-wallpapers-184.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="https://www.crystalbit.com/images/mypics/Cityscape-at-Night-12.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
 
 
-<strong>Pics Index</strong><br />
 
+    <div class="item">
+      <img src="http://www.crystalbit.com/images/mypics/sandbox-landscape-wallpaper-background-rainbow-cityscape-images-night-192311.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
 
+    ...
+  </div>
 
-
-
-
-<?php
-
-
-if ($handle = opendir(PICSPATH)) 
-{
-    while (false !== ($file = readdir($handle))) {
-        // do something with the file
-
-	if($file!='.' && $file!='..')
-	echo link_to(sprintf('<img src="%simages/mypics/%s" style="border:0; margin:0; padding:0;width:400px; height:400px;" />', BASEHREF,  $file), sprintf('%simages/mypics/%s', BASEHREF, $file)  );
-        // note that '.' and '..' is returned even
-    }
-    closedir($handle);
-}
-
-
-
-
-?>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
