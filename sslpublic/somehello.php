e2bf2af90e5477cbe6e743ecf7362fa377929808
James Tarleton <jamestarleton@gmail.com>
2014-09-04 22:21:00 -0400

    add hello

---
+++ sslpublic/somehello.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>Hello World</title>
+
+<style type="text/css">body {
+	
+	background-color:salmon;
+	color:turquoise;
+	font-family:Verdana, sans;
+} </style>
+<body>
+
+<?php 
+
+echo 'Hello World'; 
+?>
+</body>
+</html>
